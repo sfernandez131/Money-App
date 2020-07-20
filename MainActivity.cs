6ca19f5d2861e7fd1aa266a3e19b4c9b00e0577e
Scott Fernandez <sfernandez131@gmail.com>
2020-07-20 10:06:09 -0400

    App to learn Xamarin

---
+++ MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Money_App
+{
+    [Activity(Label = "Money App", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
